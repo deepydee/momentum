ff5cc08a09e9efe68aaa28aebc0b2361c8a6b6e1
Vitaliy Semenov <synthex@bk.ru>
2023-02-20 14:51:17 +0600

    feat: add todo

--- momentum/js/scripts.js
+++ momentum/js/scripts.js
@@ -22,7 +22,7 @@ const buttonPlay = document.querySelector('.play');
 const buttonNext = document.querySelector('.play-next');
 const buttonPrev = document.querySelector('.play-prev');
 const playListContainer = document.querySelector('.play-list');
-const settingsDashboard = document.querySelector('.settings');
+const settingsDashboard = document.querySelector('#settings');
 const settingsToggle = document.querySelector('.settings .toggle');
 const mobileCloseTablet = document.querySelector('.settings-tablet-control .mobile-close');
 const settingsList = document.querySelector('#apps-list');
@@ -34,10 +34,29 @@ const settingsSliderInfoAlert = document.querySelector('.bg-info');
 const customSlideTag = document.querySelector('#slide-tag');
 const buttonSubmitBgChange = document.querySelector('.settings-submit');
 
+const todo =  document.querySelector('#todo');
+const todoToggle = document.querySelector('.todo-toggle');
+const activeListContainer = document.querySelector('.active-list-container');
+const todoListChooserToggle = document.querySelector('.list-chooser-toggle');
+const todoListWrapper = document.querySelector('.todo-list-wrapper');
+const todoList = document.querySelector('.todo-list');
+const headerDropdown = document.querySelector('.todo-header .dropdown');
+const todoNewInput = document.querySelector('#todo-new');
+const todoListChooserDropdown = document.querySelector('.list-chooser');
+const todoItemTitle = document.querySelector('.todo-item-title');
+
+let todoListType = document.querySelector('.todo-list-choice-active').dataset.listId;
+
 let state = {
   language: 'en',
   photoSource: 'github',
-  blocks: ['time', 'date','greeting', 'quote', 'weather', 'player']
+  blocks: ['time', 'date','greeting', 'quote', 'weather', 'player', 'todo'],
+  todo: 
+    {
+      inbox: {},
+      today: {},
+      done: {},
+    }
 }
 let lang = state['language'];
 let isPlay = false;
@@ -48,7 +67,11 @@ let customTag = '';
 /**
  * Load/Save Settings
  */
-window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('beforeunload', () => {
+  localStorage.clear();
+  setLocalStorage();
+});
+
 window.addEventListener('load', () => {
   getLocalStorage();
   setState();
@@ -178,6 +201,203 @@ buttonSubmitBgChange.addEventListener('click', (event) => {
   : '';
 
   getLinkToImage();
+});
+
+todoToggle.addEventListener('click', () => {
+  toggleTodo();
+});
+
+todoListChooserToggle.addEventListener('click', () => {
+  toggleMainTodoMenu();
+});
+
+
+todoListChooserDropdown.addEventListener('click', function(event) {
+  const currentItem = event.target.closest('ul');
+
+  if (!currentItem) return;
+
+  todoListType = currentItem.dataset.listId;
+  const activeListName = document.querySelector('.active-list-name');
+  let listName = currentItem.querySelector('.list-name').textContent;
+
+  [...todoListChooserDropdown.children].forEach(
+    ul => ul.querySelector('li').className = 'todo-list-choice'
+  );
+
+  currentItem.querySelector('li').className = 'todo-list-choice-active';
+  activeListName.textContent = listName;
+
+  clearTodoList();
+  getTodoList(todoListType);
+
+  toggleMainTodoMenu();
+});
+
+todoList.addEventListener('click', (event) => {
+  const moreButton = event.target.closest('.more');
+  let todoListHeight = getComputedStyle(todoList).height;
+  
+  if (!moreButton) return;
+
+  let todoItemDropdown = moreButton.querySelector('.todo-item-dropdown');
+
+  if (moreButton.classList.contains('active')) {
+    moreButton.classList.remove('active');
+    moreButton.closest('li').classList.remove('active');
+
+    todoItemDropdown.style.cssText = `
+    display: none;
+    top: 0px;
+    right: 40px;
+    bottom: auto;
+    opacity: 0;
+    height: auto;`;
+
+    todoItemDropdown.firstElementChild.innerHTML = '';
+
+    todoListWrapper.style.cssText = `
+    min-height: ${ parseInt(todoListHeight) + 2}px;
+    max-height: ${ parseInt(todoListHeight) + 2}px;
+  `;
+  } else {
+    [...document.querySelectorAll('.todo-item-dropdown')].forEach((el) => {
+      el.style.cssText = `
+      display: none;
+      top: 0px;
+      right: 40px;
+      bottom: auto;
+      opacity: 0;
+      height: auto;
+      `;
+      el.firstElementChild.innerHTML = '';
+    });
+    
+    document.querySelector('.todo-item.active')?.classList.remove('active');
+    document.querySelector('.more.active')?.classList.remove('active');
+   
+    moreButton.classList.add('active');
+    moreButton.closest('li').classList.add('active');
+
+    todoItemDropdown.style.cssText = `
+    display: block;
+    top: 0px;
+    right: 40px;
+    bottom: auto;
+    opacity: 1;
+    height: auto;
+    `;
+
+    todoItemDropdown.firstElementChild.insertAdjacentHTML('afterbegin', addMoreDropdown());
+
+    todoListWrapper.style.cssText = `
+    min-height: ${ parseInt(todoListHeight) + 80}px;
+    max-height: ${ parseInt(todoListHeight) + 80}px;
+  `;
+
+  // console.log(todoItemDropdown.firstElementChild);
+    new MoreMenu(todoItemDropdown.firstElementChild);
+  }
+});
+
+todoList.addEventListener('click', function(event) {
+  let checkbox = event.target.closest('.todo-item-checkbox');
+  
+  if (!checkbox) return;
+
+  let id = checkbox.closest('li').dataset.todoId;
+
+  if (checkbox.checked) {
+    checkbox.closest('li').classList.add('done');
+
+    state.todo['done'][id] = state.todo[todoListType][id];
+    delete state.todo[todoListType][id];
+  } else {
+    checkbox.closest('li').classList.remove('done');
+    state.todo[todoListType][id] = state.todo['done'][id];
+    delete state.todo['done'][id];
+  }
+
+  refreshTodoMainDropdown();
+});
+
+class MoreMenu {
+  constructor(elem) {
+    this._elem = elem;
+    elem.onclick = this.onClick.bind(this);
+  }
+
+  edit(id) {
+    const item = document.querySelector(`[data-todo-id="${id}"] .todo-item-title`);
+    let caret = document.createRange();
+    let sel = window.getSelection();
+
+    item.setAttribute('contenteditable', true);
+    caret.selectNodeContents(item);
+    caret.collapse(null);
+    sel.removeAllRanges();
+    sel.addRange(caret);
+    item.focus();
+
+    item.addEventListener('keydown', function(event) {
+      if (event.code === 'Enter') {
+        saveItemToStorage();
+        event.preventDefault();
+      }
+    });
+
+    item.addEventListener('focusout', saveItemToStorage);
+
+    function saveItemToStorage () {
+      let newItemText = item.textContent;
+      item.setAttribute('contenteditable', false);
+      state.todo[todoListType][id] = newItemText;
+    }
+
+  }
+
+  moveInbox(id) {
+    state.todo.inbox[id] = state.todo.today[id];
+    delete state.todo.today[id];
+    clearTodoList();
+    setTodoState();
+  }
+
+  moveToday(id) {
+    state.todo.today[id] = state.todo.inbox[id];
+    delete state.todo.inbox[id];
+    clearTodoList();
+    setTodoState();
+  }
+
+  remove(id) {
+    delete state.todo[todoListType][id];
+    clearTodoList();
+    setTodoState();
+  }
+
+  onClick(event) {
+    let action = event.target.closest('li').dataset.action;
+
+    if (action) {
+      let id = event.target.closest('.todo-item').dataset.todoId;
+      this[action](id);
+    }
+  }
+}
+
+
+
+todoNewInput.addEventListener('change', function(event) {
+  let todoItem = this.value;
+
+  if (todoItem) {
+    addTodo(uuidv4(), todoItem, false);
+    this.value = '';
+    clearTodoList();
+    setTodoState();
+  }
+
 });
 
 function showTime() {
@@ -360,7 +580,7 @@ async function getQuotes(filePath) {
   const data = await res.json(); 
   let quoteNum = getRandomNum(0, data.length);
 
-  quote.textContent = data[quoteNum].text;
+  quote.textContent = `"${data[quoteNum].text}"`;
   author.textContent = data[quoteNum].author;
 }
 
@@ -442,6 +662,8 @@ function setState() {
     ? option.setAttribute('selected', 'selected')
     : option.removeAttribute('selected')
   );
+
+  setTodoState();
 }
 
 function toggleWidget(widgetName) {
@@ -454,4 +676,134 @@ function showWidget(widgetName) {
   const widget = document.querySelector(`.${widgetName}`);
 
   widget.classList.add('visible');
+}
+
+function toggleTodo() {
+  todo.classList.toggle('show-fade-in');
+  todo.classList.toggle('show');
+}
+
+function toggleMainTodoMenu() {
+  let todoListHeight = getComputedStyle(todoList).height;
+
+  activeListContainer.classList.toggle('active');
+  
+  if (activeListContainer.classList.contains('active')) {
+    todoListWrapper.style.cssText = `
+      min-height: ${ parseInt(todoListHeight) + 80}px;
+      max-height: auto;
+    `;
+  } else {
+    todoListWrapper.style.cssText = `
+      min-height: ${ parseInt(todoListHeight) + 2}px;
+      max-height: auto;
+    `;
+  }
+}
+
+function addTodo(id, title, done = false) {
+  let item = document.createElement('li');
+
+  item.className = done ?
+    "todo-item done visible" :
+    "todo-item visible";
+
+  item.setAttribute('data-todo-id', id);
+  item.setAttribute('draggable', 'true');
+
+  item.innerHTML = `
+  <span class="todo-item-wrapper has-more">
+    <label><input class="todo-item-checkbox" type="checkbox"></label>
+    <span class="todo-item-title">${title}</span>
+    <div class="more">
+      <div class="icon-wrapper more-toggle">
+        <svg class="icon icon-ellipsis more-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60"><path d="M8 22c-4.411 0-8 3.589-8 8s3.589 8 8 8 8-3.589 8-8-3.589-8-8-8zM52 22c-4.411 0-8 3.589-8 8s3.589 8 8 8 8-3.589 8-8-3.589-8-8-8zM30 22c-4.411 0-8 3.589-8 8s3.589 8 8 8 8-3.589 8-8-3.589-8-8-8z"></path>
+        </svg>
+      </div>
+      <div class="dropdown todo-item-dropdown">
+        <ul class="dropdown-list">
+        </ul>
+      </div>
+    </div>
+  </span>
+  `;
+
+  todoList.append(item);
+
+  if (!Object.keys(state.todo[todoListType]).includes(id)) {
+    state.todo[todoListType][id] = title;
+  }
+}
+
+function addMoreDropdown() {
+  let html = `
+    <li class="dropdown-list-item" data-action="edit">
+       <span class="dropdown-list-label">Редактировать</span>
+    </li>`;
+    
+    if (todoListType === 'inbox') {
+      html += `<li class="dropdown-list-item" data-action="moveToday">
+      <span class="dropdown-list-label">Перенести на сегодня</span>
+    </li>`;
+    } else if (todoListType === 'today') {
+      html += `<li class="dropdown-list-item" data-action="moveInbox">
+      <span class="dropdown-list-label">Перенести во входящие</span>
+      </li>`;
+    }
+    
+    html += `<li class="dropdown-list-item" data-action="remove">
+      <span class="dropdown-list-label">Удалить</span>
+    </li>`;
+
+    return html;
+}
+
+function setTodoState() {
+  if (state.todo) {
+
+    [...todoListChooserDropdown.children].forEach(
+      ul => {
+        let li = ul.querySelector('li');
+        let todoCount = li.querySelector('.todo-count');
+        let count = Object.keys(state.todo[li.dataset.listId]).length;
+        todoCount.textContent = count;
+
+        if (li.classList.contains('todo-list-choice-active')) {
+          getTodoList(li.dataset.listId); 
+        }
+      }
+    );
+  }
+}
+
+function refreshTodoMainDropdown() {
+  if (state.todo) {
+
+    [...todoListChooserDropdown.children].forEach(
+      ul => {
+        let li = ul.querySelector('li');
+        let todoCount = li.querySelector('.todo-count');
+        let count = Object.keys(state.todo[li.dataset.listId]).length;
+        todoCount.textContent = count;
+      }
+    );
+  }
+}
+
+function getTodoList(listId) {
+  if (Object.keys(state.todo[listId]).length > 0) {
+    for (let key in state.todo[listId]) {
+      addTodo(key, state.todo[listId][key], listId === 'done');
+    }
+  }
+}
+
+function clearTodoList() {
+  todoList.innerHTML = '';
+}
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
 }
